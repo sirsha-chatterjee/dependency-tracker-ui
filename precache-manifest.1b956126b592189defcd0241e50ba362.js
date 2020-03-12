69745fc7394ae57636197d9d0e6b0ba715ee5ba2
schatterjee6 <sirsha_chatterjee@intuit.com>
2020-03-12 11:45:21 +0530

    Updates

---
+++ precache-manifest.1b956126b592189defcd0241e50ba362.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d2e9d1b8c0b8d31d0d67128d93bf34e",
+    "url": "/dependency-tracker-ui/index.html"
+  },
+  {
+    "revision": "cf975e38fda6b6825d2a",
+    "url": "/dependency-tracker-ui/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6ff40d47189cced4793b",
+    "url": "/dependency-tracker-ui/static/css/main.f387ca1b.chunk.css"
+  },
+  {
+    "revision": "cf975e38fda6b6825d2a",
+    "url": "/dependency-tracker-ui/static/js/2.1c1069c5.chunk.js"
+  },
+  {
+    "revision": "2da2a9ed6985433f62452b08ece7b555",
+    "url": "/dependency-tracker-ui/static/js/2.1c1069c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6ff40d47189cced4793b",
+    "url": "/dependency-tracker-ui/static/js/main.ec4747bd.chunk.js"
+  },
+  {
+    "revision": "b9585e2df9403ca549d7",
+    "url": "/dependency-tracker-ui/static/js/runtime-main.6aeb3c17.js"
+  },
+  {
+    "revision": "5e00b5966716e93bc6b64ee478a1aa0b",
+    "url": "/dependency-tracker-ui/static/media/Intuit-Logo.5e00b596.png"
+  }
+]);
